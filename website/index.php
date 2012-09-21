43f258d51adf83c1fa1530aa1127ada211928d96
Burke Ramsey <birk5437@gmail.com>
2012-09-20 22:50:32 -0400

    added left sidebar

--- website/index.php
+++ website/index.php
@@ -1,7 +1,11 @@
 <html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+  </head>
   <body>
     <div id="wrapper">
       <?php include 'header.html'; ?>
+      <?php include 'sidebar_left.html'; ?>
     </div>
   </body>
 </html>
